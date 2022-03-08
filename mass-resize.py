a2bea7a5f5bf7e74ac6aa06d15ede7877145135c
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-03-08 20:07:21 -0300

    Improve output and add args.

--- mass-resize.py
+++ mass-resize.py
@@ -1,6 +1,12 @@
 from os.path import dirname,basename
 from PIL import Image
 
+def clear():
+    from os import system
+    import os
+
+    system("cls" if os.name=="nt" else "clear")
+
 def filename(path):
     import os
     from urllib.parse import urlparse
@@ -12,6 +18,8 @@ def filename(path):
 def files(path,lamb=None):
     import os
 
+    global fulllog
+    
     fold = os.walk(path)
     ret = []
 
@@ -20,16 +28,25 @@ def files(path,lamb=None):
     for root, dirs, files in fold:
         if(once):
             base = (100/len(files))
+            
             bar = "█"
             blank = " "
+
             i = 0
+            last = 0
+
         for name in files:
             if(lamb != None):
                 lamb(os.path.realpath(os.path.join(root, name)))
                 if(once):
                     i+=1
-                    print("|"+bar*int(base*i/3), end="")
-                    print(blank*(int(base*len(files)/3)-int(base*i/3))+"|")
+                    if(base*i-last > 5):
+                        clear()
+                        print(fulllog)
+                        print("|"+bar*int(base*i/3), end="")
+                        print(blank*(int(base*len(files)/3)-int(base*i/3))+"|")
+                        
+                        last = base*i
             
             ret.append(os.path.realpath(os.path.join(root, name)))
         
@@ -37,33 +54,58 @@ def files(path,lamb=None):
      
     return ret
 
-def resize(f):
+def resize(f,percent):
+    global fulllog
+
     scale = lambda x,y: (int((x[0]/100)*y),int((x[1]/100)*y))
     
     try:
         img = Image.open(f)
-        img = img.resize(scale(img.size,50))
+        img = img.resize(scale(img.size,percent))
         img.save(f)
 
-        print(f+": ok")
+        fulllog+=log(f+": ok")
     except:
-        print("error on file :"+f)
+        fulllog+=log("error on file :"+f)
+
+def log(text):
+    text = text.upper()
+    print(text)
+
+    return text+"\n"
+
+fulllog = ""
 
 def run():
     from os.path import exists, realpath
     from os import mkdir, chdir
     from shutil import rmtree as rmdir
     from shutil import copytree as copy
+    import argparse
+
+    global fulllog
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("path")
+    parser.add_argument("percent")
+    
+    args = parser.parse_args()
     
     if(exists("output")):
+        fulllog+=log("removing output...")
         rmdir("output")
 
-    print("making a copy...")
-    copy("f:/img","output")
+    fulllog+=log("making a copy...")
     
-    print(realpath("output"))
+    copy(args.path,"output")
+    
+    fulllog+= log("rescaled images will be in :"+realpath("output")+".") 
 
-    print("starting...")
-    files("output", resize)
+    fulllog+=log("starting...\n")
+    
+    callback = lambda file,percent=int(args.percent.replace("%","")): resize(file,percent)
+
+    files("output", callback)
 
 run()
