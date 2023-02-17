c4fa403b20ce250bdce04c88c07bd7136ee4012e
allansm <37338692+allansm@users.noreply.github.com>
2023-02-16 23:33:42 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import platform
+
+modules=[
+    "Pillow"
+]
+
+setup(
+    name="mass-resize",
+    version="2023.2.16",
+    packages=[],
+
+    install_requires=modules
+)
